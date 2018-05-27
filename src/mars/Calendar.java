5ecdf12334876142e2a99335e6a92ff4612d8c77
yhs21c <yhs21c@maum.org>
2018-05-27 13:27:43 +0900

    exption -1,13

--- src/mars/Calendar.java
+++ src/mars/Calendar.java
@@ -12,12 +12,24 @@ public class Calendar {
 	}
 	
 	public static void main(String[] args) {
+		String prompt="input>";
 		Scanner scan = new Scanner(System.in);
 		Calendar cal = new Calendar();
+		
+	while (true) {
 		System.out.println("원하는 달은?");	
+		System.out.println(prompt);	
 		int mon = scan.nextInt();
-	
-        System.out.printf("%d월은 %d일까지 있어요",mon,cal.getMaxDay(mon));
+
+		if (mon==-1){
+			break;
+		}
+		if (mon>12){
+			continue;
+		}
+		System.out.printf("%d월은 %d일까지 있어요\n",mon,cal.getMaxDay(mon));
+
+	}
 		
 		scan.close();
 
